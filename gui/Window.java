35ebc21e648a101e183ab24ecbfeaf781c8c8076
LePaulENSG <p.miancien@laposte.net>
2022-03-07 16:17:39 +0100

    Show score

--- gui/Window.java
+++ gui/Window.java
@@ -4,22 +4,48 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Window {
-    public Window() {
-        //1. Create the frame.
+    int size = 3;
+
+
+    public Window(int size) {
+        this.size = size;
+
+        // Create the frame.
         JFrame frame = new JFrame("Tic-tac-toe");
 
-        //2. Optional: What happens when the frame closes?
+        // Optional: What happens when the frame closes?
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //3. Create components and put them in the frame.
-        //...create emptyLabel...
-        JLabel emptyLabel = new JLabel("");
-        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+        // Create components and put them in the frame.
+        // Create panel with score
+        JLabel P1Label = new JLabel("Player 1");
+        P1Label.setSize(150,50);
+        JLabel score = new JLabel("0");
+        score.setSize(50,50);
+        JLabel P2Label = new JLabel("Player 2");
+        P2Label.setSize(150,50);
 
-        //4. Size the frame.
-        frame.setSize(250, 250);
+        JPanel scorePanel = new JPanel();
+        scorePanel.add(P1Label);
+        scorePanel.add(score);
+        scorePanel.add(P2Label);
 
-        //5. Show it.
+        frame.getContentPane().add(scorePanel, BorderLayout.NORTH);
+
+        // Add boxes
+        for (int i=0; i<size;i++) {
+            for (int j = 0; j < size; j++) {
+                Box box = new Box();
+                //frame.getContentPane().add(box);
+            }
+        }
+
+        // Size the frame.
+        frame.setSize(500, 500);
+
+
+
+        // Show it.
         frame.setVisible(true);
     }
 
